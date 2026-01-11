ca4531ebbb84067263f0f27b91123a5afcaa0784
shems212121-droid <shems212121@gmail.com>
2026-01-12 01:03:14 +0300

    Fix: Enable DEBUG temporarily to diagnose 500 error

--- electoral_office/settings_production.py
+++ electoral_office/settings_production.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@z_(zqnz11&19e9=1c*b8fvaekk_gs=6*)($x^a$sx@pyl2o88')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True # os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
