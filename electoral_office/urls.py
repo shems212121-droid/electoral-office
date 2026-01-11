35bc197488795a872d61cdaa8fca3463bc443d14
shems212121-droid <shems212121@gmail.com>
2026-01-12 01:33:48 +0300

    Debug: Add /test-ping/ route to verify basic server functionality

--- electoral_office/urls.py
+++ electoral_office/urls.py
@@ -3,7 +3,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.http import HttpResponse
+
+def ping(request):
+    return HttpResponse("Pong - System is Active")
+
 urlpatterns = [
+    path('test-ping/', ping),
     path('admin/', admin.site.urls),
     path('finance/', include('finance.urls')),
     path('archive/', include('archive.urls')),
